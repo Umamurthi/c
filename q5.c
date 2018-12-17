42e139365abc2a7e226c9c425c573388dd544d68
Umamurthi <45891084+Umamurthi@users.noreply.github.com>
2018-12-17 12:26:05 +0530

    5 - Find the largest number among three numbers entered by user.

---
+++ q5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+	double num1, num2, num3;
+
+    printf("Enter three numbers: ");
+    scanf("%lf %lf %lf", &num1, &num2, &num3);
+
+    if (num1>=num2)
+    {
+        if(num1>=num3)
+            printf("%.2lf is the largest number.", num1);
+        else
+            printf("%.2lf is the largest number.", num3);
+    }
+    else
+    {
+        if(num2>=num3)
+            printf("%.2lf is the largest number.", num2);
+        else
+            printf("%.2lf is the largest number.", num3);
+    }
+	return 0;
+}
