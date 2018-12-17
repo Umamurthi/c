782849e0c272b14008b1083ce58abdd9eba6cba0
Umamurthi <45891084+Umamurthi@users.noreply.github.com>
2018-12-17 12:41:17 +0530

    6 - Check whether the year is a leap year or not.

---
+++ q6.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+    int year;
+    scanf("%d",&year);
+    if(((year%400==0)&&(year%100==0))||(year%4)==0) printf("leap year");
+    else printf("not a leap year");
+    return 0;
+}
