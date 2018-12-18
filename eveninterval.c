eb6cf7dfba4bbb9acc59af82d192e2f7591d2176
Umamurthi <45891084+Umamurthi@users.noreply.github.com>
2018-12-18 14:48:10 +0530

    even number between interval

---
+++ eveninterval.c
@@ -0,0 +1,18 @@
+int i,start,end; 
+
+   scanf("%d%d",&start,&end);
+if(start%2 != 0)
+    {
+        start++;
+    }
+
+   
+    for(i=start; i<=end; i+=2)
+    {
+        printf("%d\n",i);
+    }
+   
+
+
+	return 0;
+}
